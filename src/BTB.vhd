26b88e32c96f6a06fa3f081592028a9d5806379a
GerryM-97 <96832927+GerryM-97@users.noreply.github.com>
2022-02-09 17:03:14 +0100

    Update BTB.vhd

--- src/BTB.vhd
+++ src/BTB.vhd
@@ -19,7 +19,7 @@ PORT (
 		--outputs
 		MATCH : out std_logic;
 		BRANCH_TARGET : out address;
-		PREDICTION : out std_logic_vector(1 DOWNTO 0)
+		PREDICTION : out std_logic
 );
 END ENTITY;
 
@@ -27,7 +27,7 @@ ARCHITECTURE struct OF BTB IS
 
 TYPE BHT_ARR IS ARRAY (0 TO 31) OF address;
 TYPE TARGET_ADD_ARR IS ARRAY (0 TO 31) of address;
-TYPE PREDICTOR_ARR is array (0 TO 31) of std_logic_vector(1 DOWNTO 0);
+TYPE PREDICTOR_ARR is array (0 TO 31) of std_logic;
 
 SIGNAL BHT : BHT_arr;
 SIGNAL TARGET_ADD : target_add_arr;
@@ -77,27 +77,11 @@ pred_proc : PROCESS(RST, CLK)
 BEGIN
 		IF RST = '1' THEN
 			PREDICTOR <= (OTHERS => (OTHERS => '0'));
-		ELSIF RISING_EDGE(CLK) THEN
-			IF PRED_T_T = '1' THEN
-				IF PREDICTOR(ENTRY_ID) = "10" THEN
-					PREDICTOR(ENTRY_ID) <= "11";
-				END IF;
-			ELSIF PRED_T_NT = '1' THEN
-				IF PREDICTOR(ENTRY_ID) = "11" THEN
-					PREDICTOR(ENTRY_ID) <= "10";
-				ELSE
-					PREDICTOR(ENTRY_ID) <= "00";
-				END IF;
+		ELSIF RISING_EDGE(CLK) THEN		
+			IF PRED_T_NT = '1' THEN				
+				PREDICTOR(ENTRY_ID) <= "0";
 			ELSIF PRED_NT_T = '1' THEN
-				IF PREDICTOR(ENTRY_ID) = "00" THEN
-					PREDICTOR(ENTRY_ID) <= "01";
-				ELSE
-					PREDICTOR(ENTRY_ID) <= "11";
-				END IF;
-			ELSIF PRED_NT_NT = '1' THEN
-				IF PREDICTOR(ENTRY_ID) = "01" THEN
-					PREDICTOR(ENTRY_ID) <= "00";
-				END IF;
+				PREDICTOR(ENTRY_ID) <= "1";	
 			END IF;
 		END IF;
 END PROCESS;
